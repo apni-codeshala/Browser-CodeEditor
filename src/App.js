963b80dff853ef720fe7fb1e94f68735411df3b7
Abhishek Patel <127352245+apni-codeshala@users.noreply.github.com>
2023-11-06 21:09:52 +0530

    Started the project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);

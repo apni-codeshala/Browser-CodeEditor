f2332c68fdd7cbea5d4c65a8436c6e2e020dfe19
Abhishek Patel <127352245+apni-codeshala@users.noreply.github.com>
2023-11-07 23:25:49 +0530

    Completed code functionality with outhput window

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
-import ReactDOM from "react-dom/client";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import Editor from "./components/Editor";
 
 const App = () => {
+
+    const [html, setHtml] = useState("")
+    const [css, setCss] = useState("")
+    const [js, setJs] = useState("")
+    const [srcDoc, setSrcDoc] = useState("")
+
+
+    // The html, css and js on every click this slow down our page so we will do it after some interval
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setSrcDoc(`
+            <html>
+                <body>${html}</body>
+                <style>${css}</style>
+                <script>${js}</script> 
+            </html>
+            `)
+        }, 250);
+
+        return () => clearTimeout(timeout);
+    }, [html, css, js])
+
+
     return (
-        <h1>Hello World</h1>
+        <>
+            <div className="pane top-pane">
+                <Editor
+                    language="xml"
+                    displayName="HTML"
+                    value={html}
+                    onChange={setHtml}
+                />
+                <Editor
+                    language="css"
+                    displayName="CSS"
+                    value={css}
+                    onChange={setCss}
+                />
+                <Editor
+                    language="javascript"
+                    displayName="JS"
+                    value={js}
+                    onChange={setJs}
+                />
+            </div>
+            <div className="pane">
+                <iframe
+                    srcDoc={srcDoc}  // This is the most important this this loads html in iframe
+                    title="output"
+                    sandbox="allow-scripts allow-same-origin"
+                    frameBorder="0"
+                    width="100%"
+                    height="100%"
+                />
+            </div>
+        </>
     )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
